3b896da9d188f7fc2fdbd3a42f9c3f60c2a66205
gabbyk77 <122gkulick@frhsd.com>
2022-03-28 12:04:32 -0400

    changed for loop

--- site.js
+++ site.js
@@ -43,10 +43,10 @@ const vue_app = Vue.createApp({
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
             getMonthText(dateArray) {
               //not really sure what to put in these functions yet, or if they're even in the right spots
-            }
+            },
             like(index) {
 
-            }
+            },
             dislike(index) {
 
             }

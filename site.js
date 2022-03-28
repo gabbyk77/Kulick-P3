63468fca8e40a4676f0d0b814a2da4e4c237c615
gabbyk77 <122gkulick@frhsd.com>
2022-03-28 12:52:09 -0400

    step 7 switch statement, and tried to make like and dislike buttons work

--- site.js
+++ site.js
@@ -42,12 +42,42 @@ const vue_app = Vue.createApp({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
             getMonthText(dateArray) {
-              //not really sure what to put in these functions yet, or if they're even in the right spots
+              switch (dateArray[1]) {
+                case 1: month = "January";
+                break;
+                case 2: month = "February";
+                break;
+                case 3: month = "March";
+                break;
+                case 4: month = "April";
+                break;
+                case 5: month = "May";
+                break;
+                case 6: month = "June";
+                break;
+                case 7: month = "July";
+                break;
+                case 8: month = "August";
+                break;
+                case 9: month = "September";
+                break;
+                case 10: month = "October";
+                break;
+                case 11: month = "November";
+                break;
+                case 12: month = "December";
+                break;
+                return month;
+            }
+
             },
             like(index) {
 
             },
             dislike(index) {
+
+            },
+            posterClick(index) {
 
             }
       }
